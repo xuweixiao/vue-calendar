9f132d5353d7bed1e55b89732d62659017a0a86f
徐丹锋 <xudanfeng@huatu.com>
2019-05-31 13:15:46 +0800

    vue2.6.0以上插槽demo

---
+++ src/components/table.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>
+        <div class="header">
+            <slot name="pageHeader" v-bind:info="title">
+                默认头部
+            </slot>
+        </div>
+        <div class="main">
+            <slot name="main"></slot>
+        </div>
+        <div class="footer">
+            <slot name="pageFooter">
+                默认底部
+            </slot>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '子组件表格头部'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.header{
+    color: #35abdd;
+    text-align: center;
+    padding: 10px;
+}
+.table{
+    padding: 10px;
+    border: 1px solid #c1baba;
+
+    .table-item{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        border-bottom: 1px solid #c1baba;
+
+        span {
+            display: block;
+            text-align: center;
+            padding: 10px;
+        }
+    }
+}
+.footer{
+    margin-top: 20px;
+    text-align: center;
+    padding: 10px;
+    color: #a8b6bc;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,28 @@
             :timeData="timeFilterData">
         </search-condition>
         <div class="body">
+            <!-- 1.时间筛选器 -->
             <h3>{{this.timeFilterData.customDateStr}}</h3>
+            <!-- 2.插槽使用 -->
+            <section>
+                <my-table>
+                    <template #pageHeader="propsData">
+                        ----插槽定制头部----
+                        <div>{{propsData.info}}</div>
+                    </template>
+                    <template #main>
+                        <ul class="table">
+                        <li
+                            class="table-item"
+                            v-for="(item,index) of peopleArr"
+                            :key="index +'peo'">
+                            <span>{{item.name}}</span>
+                            <span>{{item.age}}</span>
+                        </li>
+                        </ul>
+                    </template>
+                </my-table>
+            </section>
         </div>
     </div>
 </template>
@@ -21,16 +42,24 @@
 // @ is an alias to /src
 import searchCondition from '@/components/searchCondition.vue'
 import pageProgress from '@/components/pageProgress.vue'
+import myTable from '@/components/table.vue'
 
 export default {
   name: 'home',
   components: {
+    myTable,
     searchCondition,
     pageProgress
   },
   data () {
     return {
-      timeFilterData: {}
+      timeFilterData: {},
+      peopleArr: [
+        { name: '徐开心', age: 13 },
+        { name: '大韩', age: 6 },
+        { name: '民国', age: 6 },
+        { name: '万岁', age: 6 }
+      ]
     }
   },
   computed: {
